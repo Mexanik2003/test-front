d6d1aff3e51c61cd9164f2a4010001c06dcd79b6
AD <ad@it-eburg.ru>
2021-09-14 21:21:30 +0500

    layout applied

--- art-test/src/app/app.module.ts
+++ art-test/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './components/app/app.component';
 import { CompanyListComponent } from './components/company-list/company-list.component';
@@ -9,10 +9,47 @@ import { CompanyYandexMapComponent } from './components/company-yandex-map/compa
 import { CompanyItemComponent } from './components/company-item/company-item.component';
 import { CompanySortComponent } from './components/company-sort/company-sort.component';
 import { CompanyFilterComponent } from './components/company-filter/company-filter.component';
+import { LayoutComponent } from './layouts/layout/layout.component'
+
+
+const routes: Routes = [  
+  { 
+    path: '', 
+    component: LayoutComponent, 
+    children: [
+      { 
+        path: '', 
+        component: CompanyListComponent 
+      }
+    ]
+  },
+  { 
+    path: 'detail', 
+    component: LayoutComponent, 
+    children: [
+      { 
+        path: '', 
+        component: CompanyDetailComponent 
+      }
+    ]
+  },
+  { 
+    path: 'map', 
+    component: LayoutComponent, 
+    children: [
+      { 
+        path: '', 
+        component: CompanyYandexMapComponent 
+      }
+    ]
+  },
+];
+
 
 
 @NgModule({
   declarations: [
+    LayoutComponent,
     AppComponent,
     CompanyListComponent,
     CompanyDetailComponent,
@@ -23,7 +60,7 @@ import { CompanyFilterComponent } from './components/company-filter/company-filt
 ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
